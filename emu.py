c80d84dc2c5737f53ef7144872e46f4c9604c2ae
jeressi2 <jeressi@mail.ru>
2025-09-03 17:47:49 +0300

    1st stage

--- emu.py
+++ emu.py
@@ -1,1 +1,16 @@
-print ("Hello, World!")
+import shlex
+vfs_name = "docker$ "
+exit_cmd = "exit"
+while True:
+    command = input(vfs_name)
+    parts = shlex.split(command)
+    if command == exit_cmd:
+        break
+    if len(parts) == 0:
+        continue
+    if parts[0] == 'ls':
+        print(parts)
+    elif parts[0] == 'cd':
+        print(parts)
+    else:
+        print(f'{command}: command not found')
