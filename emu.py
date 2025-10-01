c76028eaf7d42fd277ee5f96229f21fae6ec4bba
jeressi2 <jeressi@mail.ru>
2025-10-01 16:45:00 +0300

    1st stage full complete

--- emu.py
+++ emu.py
@@ -6,34 +6,34 @@ exit_cmd = "exit"
 
 def act(a):
     parts = shlex.split(command)
+
     if command == exit_cmd:
         exit()
     if len(parts) == 0:
-        return ""
+        print("")
     if parts[0] == 'ls':
         p = os.listdir(path=".") # файлы в текущей директории
-        return p
+        print(p)
     elif parts[0] == 'cd':
         if len(parts) > 1:
-            if parts[1] == "$HOME":
-                os.chdir(s)
             target_name = parts[1]
-            current_path = os.getcwd()
-            path_parts = current_path.split(os.sep) # делим директорию на части
-            for i in range(len(path_parts)):
-                if target_name == path_parts[i]:
-                    target_path = os.sep.join(path_parts[:i + 1]) # обрезаем все после нашей директории
-                    os.chdir(target_path)
-                    if target_path == 'C:':
-                        target_path += '\\'
-                        os.chdir(target_path)
-        return os.getcwd()
+            if os.path.isdir(target_name):
+                os.chdir(target_name)
+            elif parts[1] == "$HOME":
+                os.chdir(s)
+            else:
+                print(target_name)
+        else:
+            a = os.path.expanduser("~")
+            os.chdir(a)
+    elif parts[0] == 'pwd':
+        print(os.getcwd())
     else:
-        return f'{command}: command not found'
+        print(f'{command}: command not found')
 
 if __name__ == "__main__":
     s = os.getcwd()  # текущая директория
     print(f'Текущая директория: {s}')
     while True:
         command = input(f'{vfs_name}$ ')
-        print(act(command))
+        act(command)
